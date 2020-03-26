45c50ffacbc077461113b1a46abaae231f1ff271
Swiftly <ivecis@live.co.kr>
2020-03-26 18:14:37 +0900

    Updates

---
+++ precache-manifest.c16e62537d7c41c752ed3f071d897eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900413d388b9b79b374b51a2bc75b57d",
+    "url": "/SwiftlyAside/enchant/index.html"
+  },
+  {
+    "revision": "4003a377866f26f9bb6e",
+    "url": "/SwiftlyAside/enchant/static/css/main.06cc566a.chunk.css"
+  },
+  {
+    "revision": "4db1fcd846f022685b44",
+    "url": "/SwiftlyAside/enchant/static/js/2.6e07e3c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SwiftlyAside/enchant/static/js/2.6e07e3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4003a377866f26f9bb6e",
+    "url": "/SwiftlyAside/enchant/static/js/main.88e6036e.chunk.js"
+  },
+  {
+    "revision": "73da53307ace5b3bb703",
+    "url": "/SwiftlyAside/enchant/static/js/runtime-main.1ef91cc9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SwiftlyAside/enchant/precache-manifest.c16e62537d7c41c752ed3f071d897eee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SwiftlyAside/enchant/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
